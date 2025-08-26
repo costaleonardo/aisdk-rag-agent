6aafa7787824a5cf97f52b91b3337d28dc0a88d5
Leonardo Da Costa <costaleonardo97@gmail.com>
2025-08-26 18:04:57 +0530

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+/**
+ * 
+ * @TODO 
+ * 
+ * 1. create a table in our database to store embeddings
+ * 2. add logic to chunk and create embeddings when creating resources
+ * 3. create an agent
+ * 4. give the agent tools to query / create resources for it's knolwedgbase
+ */
+
 export default function Chat() {
   return <main>Hello, world!</main>;
 }
